3ab2ce2109c5b41e29d2aeb59d5ca78aec0857fd
Christopher Neal <christopher.m.neal@gmail.com>
2021-10-23 11:49:02 -0700

    WIP: Click result item to copy

--- js/capchange-code.js
+++ js/capchange-code.js
@@ -1,4 +1,7 @@
 function capChange(input) {
+  if (input === "") {
+    input = "please enter some text to convert!";
+  }  
   input = input.toLowerCase();
   let inputArray = input.split("");
   for (let i = 0; i < input.length; i++) {
@@ -25,7 +28,7 @@ $(document).ready(function() {
   $("#form1").submit(function(event) {
     event.preventDefault();
     const userResult = capChange($("#text-input").val());
-    $("#result-list").prepend("<li>" + userResult + "</li>");
+    $("#result-list").prepend("<li class='result-li'>" + userResult + "</li>");
 
     $("#title-change").text(capChange("capchange code"));
     $("#header-change").text(capChange("make your text look like this!!"));
@@ -33,6 +36,14 @@ $(document).ready(function() {
     $("#submit-change").text(capChange("submit!"));
     $("#reload-change").text(capChange("click here to reset the page"));
   });
+
+  // Code to copy result-li item by clicking not working very well.
+  // Only test li in index will copy by highlighting then clicking
+  
+  // $(".result-li").click(function() {
+  //   $(".result-li").select();
+  //   document.execCommand("copy");
+  // });
 
   $("#reload-change").click(function() {
     $("#form1").val("");
